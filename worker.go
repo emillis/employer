710bd3550243930b7cb60b324212f30c672aa88a
Emilis <emilis.mitrikas@gmail.com>
2022-06-06 21:27:31 +0100

    Added Fire() and DirectWork(work TWork) methods to the worker struct. They do exactly what the name suggests. Also creating workerGoroutine function that will be spawned as a goroutine for each worker. Also, newWorker function has been edited to spawn a goroutine for each new worker. In addition to that, removed mutex from worker struct and instead, using atomic.AddInt64() to increment worker ID

--- worker.go
+++ worker.go
@@ -1,35 +1,64 @@
 package employer
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 //===========[STATIC/CACHE]====================================================================================================
+
+var uniqueIdCounter int64 = 0
 
 //===========[STRUCTS]====================================================================================================
 
 type worker[TWork any] struct {
 	//Provides direct access to the worker
-	directAccess chan <- TWork
+	directAccess chan TWork
 
-	//Fire this worker (terminates the go routine)
+	//If this channel is closed, go routine gets terminated
 	fire chan struct{}
 
 	//ID of the worker
 	id int
-
-	//Locks for this worker
-	mx sync.RWMutex
 }
 
-func (w *worker[TW])
+//DirectWork sends some work directly to this worker
+func (w *worker[TWork]) DirectWork(work TWork) {
+	w.directAccess <- work
+}
 
+//Fire makes this worker redundant
+func (w *worker[TWork]) Fire() {
+	close(w.fire)
+}
 
 //===========[FUNCTIONALITY]====================================================================================================
 
-func newWorker[TWork any]() *worker[TWork] {
-	w := &worker[TWork]{
-		directAccess: make(chan <- TWork),
-		fire: make(chan struct{}),
+//workerGoroutine is the goroutine that will be spawned for each worker. As an argument you must pass in worker struct
+func workerGoroutine[TWork any](w *worker[TWork], workPile chan TWork) {
+	if w == nil {
+		return
 	}
+
+	for {
+		select {
+		case work := <-w.directAccess:
+		case work := <-workPile:
+		case <-w.fire:
+			break
+		}
+	}
+}
+
+func newWorker[TWork any](workPile chan TWork) *worker[TWork] {
+
+	w := &worker[TWork]{
+		directAccess: make(chan TWork, 2),
+		fire:         make(chan struct{}),
+		id:           int(atomic.AddInt64(&uniqueIdCounter, 1)),
+	}
+
+	go workerGoroutine(w, workPile)
 
 	return w
 }
