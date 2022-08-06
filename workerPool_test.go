abbb7fcf88dbcc7164762723b0ca5cf05063d70e
Emilis <emilis.mitrikas@gmail.com>
2022-08-06 20:39:13 +0100

    Added the first test, testing public func "New(*Requirements)"

--- workerPool_test.go
+++ workerPool_test.go
@@ -1,2 +1,19 @@
 package workerPool
 
+import "testing"
+
+//===========[TESTS]====================================================================================================
+
+func TestNew(t *testing.T) {
+	wpNoReq := New[string](nil)
+	wpWithReq := New[string](&Requirements{
+		MinWorkers:            10,
+		MaxWorkers:            100,
+		WorkBucketSize:        25,
+		WorkerSpawnMultiplier: 10,
+	})
+
+	if wpNoReq == nil || wpWithReq == nil {
+		t.Errorf("Expected to receive non nil values from function New(), got %T and %T", wpNoReq, wpWithReq)
+	}
+}
