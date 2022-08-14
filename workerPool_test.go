04b9e52371ef2b3782c78ccae95875fc936fb540
Emilis <emilis.mitrikas@gmail.com>
2022-08-14 20:42:20 +0100

    Added test that checks automatic worker pool size increasing/shrinking based on the amount of work in the bucket

--- workerPool_test.go
+++ workerPool_test.go
@@ -3,13 +3,14 @@ package workerPool
 import (
 	"sync"
 	"testing"
+	"time"
 )
 
 //===========[TESTS]====================================================================================================
 
 func TestNew(t *testing.T) {
-	wpNoReq := NewWorkerPool[string](func(Worker, string) {}, nil)
-	wpWithReq := NewWorkerPool[string](func(Worker, string) {}, &Requirements{
+	wpNoReq := NewWorkerPool[string](func(string) {}, nil)
+	wpWithReq := NewWorkerPool[string](func(string) {}, &Requirements{
 		MinWorkers:            10,
 		MaxWorkers:            100,
 		WorkBucketSize:        25,
@@ -22,8 +23,8 @@ func TestNew(t *testing.T) {
 }
 
 func TestWorkerPool_WorkerCount(t *testing.T) {
-	wp1 := NewWorkerPool[string](func(Worker, string) {}, nil)
-	wp2 := NewWorkerPool[string](func(Worker, string) {}, &Requirements{
+	wp1 := NewWorkerPool[string](func(string) {}, nil)
+	wp2 := NewWorkerPool[string](func(string) {}, &Requirements{
 		MinWorkers:            10,
 		MaxWorkers:            20,
 		WorkBucketSize:        10,
@@ -42,31 +43,10 @@ func TestWorkerPool_WorkerCount(t *testing.T) {
 	}
 }
 
-//func TestWorkerPool_WorkHandler(t *testing.T) {
-//	result := ""
-//	wp1 := NewWorkerPool[string](nil)
-//	expectedResult := "this_is_working"
-//	wg := sync.WaitGroup{}
-//
-//	wp1.WorkHandler(func(w Worker, work string) {
-//		wg.Done()
-//		result = work
-//	})
-//
-//	wg.Add(1)
-//	wp1.AddWork(expectedResult)
-//
-//	wg.Wait()
-//
-//	if result == "" {
-//		t.Errorf("Variable result should be \"%s\", got \"%s\"", expectedResult, result)
-//	}
-//}
-
 func TestWorkerPool_AddWork(t *testing.T) {
 	result := ""
 	wg := sync.WaitGroup{}
-	wp1 := NewWorkerPool[string](func(w Worker, work string) {
+	wp1 := NewWorkerPool[string](func(work string) {
 		wg.Done()
 		result = work
 	}, nil)
@@ -82,25 +62,44 @@ func TestWorkerPool_AddWork(t *testing.T) {
 	}
 }
 
-func TestWorker_Id(t *testing.T) {
-	wg := sync.WaitGroup{}
-	result := 0
-	wp := NewWorkerPool[string](func(w Worker, work string) {
-		wg.Done()
-		result = w.Id()
-	}, nil)
-	requiredResult := 1
-
-	wg.Add(1)
-	wp.AddWork("x1")
-
-	wg.Wait()
-
-	if result != requiredResult {
-		t.Errorf("Required result is %d, got %d", requiredResult, result)
-	}
-}
-
 func TestWorker_SetWorkHandler(t *testing.T) {
 
+}
+
+func TestWorkerPool_AutoWorkerManagement(t *testing.T) {
+	wp := NewWorkerPool[int](func(work int) {
+		time.Sleep(time.Millisecond * 25)
+	}, &Requirements{
+		MinWorkers:            1,
+		MaxWorkers:            25,
+		WorkBucketSize:        500,
+		WorkerSpawnMultiplier: 25,
+		Timeout:               time.Millisecond * 50,
+	})
+
+	initialPoolSize := wp.WorkerCount()
+
+	for i := 0; i < 100; i++ {
+		wp.AddWork(i)
+	}
+
+	time.Sleep(time.Millisecond * 100)
+
+	midPoolSize := wp.WorkerCount()
+
+	time.Sleep(time.Millisecond * 250)
+
+	endPoolSize := wp.WorkerCount()
+
+	if initialPoolSize != 1 {
+		t.Errorf("Initially expected to have 1 worker, got %d", initialPoolSize)
+	}
+
+	if midPoolSize != 25 {
+		t.Errorf("Mid way through the test expected to have 25 workers, got %d", midPoolSize)
+	}
+
+	if endPoolSize != 1 {
+		t.Errorf("At the end of the test expected to have the worker count dropped back to 1, got %d", endPoolSize)
+	}
 }
